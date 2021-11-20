213daf1e501929a575fb50b4bb00028809886e6f
Arkadiusz Noster <x-arkadiusz.noster@upvest.co>
2021-11-20 20:41:59 +0100

    add producer and consumer interfaces definitions

---
+++ producer/producer.go
@@ -0,0 +1,12 @@
+package producer
+
+import (
+	"context"
+
+	"google.golang.org/protobuf/proto"
+)
+
+// Producer defines an interface for a blocking operation of adding message to the configured pdcl topic
+type Producer interface {
+	Produce(ctx context.Context, message proto.Message) error
+}
