ecd66e9b3d142423c3a036a4654755d2d8940dc6
Arkadiusz Noster <x-arkadiusz.noster@upvest.co>
2021-11-23 14:30:34 +0100

    chore(storage): refactor storage

--- storage/storage.go
+++ storage/storage.go
@@ -2,14 +2,14 @@ package storage
 
 import (
 	"context"
-	"errors"
-
+	"fmt"
 	"github.com/ipfs/go-cid"
+	"github.com/multiformats/go-multihash"
 	"google.golang.org/protobuf/proto"
 )
 
 type MessageWriter interface {
-	Write(ctx context.Context, message proto.Message) (cid.Cid, error)
+	Write(ctx context.Context, message proto.Message )  (cid.Cid, error)
 }
 
 type MessageReader interface {
@@ -27,11 +27,56 @@ type MessageStorage interface {
 	MessageWriter
 }
 
-type Error error
 
-var (
-	ErrMarshall   = errors.New("error when marshalling message")
-	ErrUnmarshall = errors.New("error when unmarshalling message")
-	ErrInternal   = errors.New("internal storage error")
-	ErrNotFound   = errors.New("message not found")
-)
+type ProtoMessageStorage struct{
+	contentStorage ContentStorage
+	marshalOpts     proto.MarshalOptions
+}
+
+func NewProtoMessageStorage(contentStorage ContentStorage) *ProtoMessageStorage {
+	return &ProtoMessageStorage{
+		contentStorage: contentStorage,
+		marshalOpts: proto.MarshalOptions{
+		Deterministic:     true,
+	}}
+}
+
+func (p *ProtoMessageStorage) Read(ctx context.Context, cid cid.Cid) (ProtoUnmarshallable, error) {
+	content, err := p.contentStorage.Read(ctx, cid)
+	if err != nil {
+		return nil, fmt.Errorf("read message from content storage: %w", err)
+	}
+	return unmarshallable{
+		protoBuf: content,
+		options:  proto.UnmarshalOptions{
+			DiscardUnknown:    true,
+		},
+	}, nil
+}
+
+func (p *ProtoMessageStorage) Write(ctx context.Context, message proto.Message) (cid.Cid, error) {
+	encoded, err := p.marshalOpts.Marshal(message)
+	if err != nil {
+		return cid.Cid{}, fmt.Errorf("marshall message: %w", err)
+	}
+
+	hash, err := multihash.Sum(encoded, multihash.SHA2_256, -1)
+	if err != nil {
+		return cid.Cid{}, fmt.Errorf("get SHA256 multihash sum from mashalled message: %s", err)
+	}
+	cidValue := cid.NewCidV1(multihash.SHA2_256, hash)
+	if err := p.contentStorage.Write(ctx, encoded, cidValue); err != nil {
+		return cid.Cid{}, fmt.Errorf("write message to content storage: %w", err)
+	}
+	return cidValue, nil
+}
+
+type unmarshallable struct{
+	protoBuf []byte
+	options proto.UnmarshalOptions
+}
+
+func (u unmarshallable) Unmarshall(message proto.Message) error {
+	return u.options.Unmarshal(u.protoBuf, message)
+}
+
