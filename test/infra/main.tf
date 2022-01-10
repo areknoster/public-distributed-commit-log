a1236ae8e0b0739bd5d3980a905ce2d34c9cd63c
Arkadiusz Noster <x-arkadiusz.noster@upvest.co>
2022-01-10 19:42:12 +0100

    test(infra): setup GCP project for acceptance testing

---
+++ test/infra/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
---
+++ test/infra/network.tf
@@ -0,0 +1,21 @@
+resource "google_compute_network" "pdcl_test_network" {
+  name                    = "pdcl-test-network"
+  auto_create_subnetworks = false
+}
+resource "google_compute_subnetwork" "pdcl_test_subnetwork" {
+  name          = "test-subnetwork-${var.region}"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = var.region
+  network       = google_compute_network.pdcl_test_network.id
+}
+
+resource "google_compute_firewall" "allow_ssh_to_ipfs_server" {
+  name = "ssh-ipfs"
+  network = google_compute_network.pdcl_test_network.name
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+  target_tags = ["ipfs-server"]
+  source_ranges = ["0.0.0.0/0"]
+}
