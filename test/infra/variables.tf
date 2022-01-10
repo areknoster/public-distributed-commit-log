f57c9d0e44315d6c8508cd9952f99b3abffddc54
Arkadiusz Noster <x-arkadiusz.noster@upvest.co>
2022-01-10 21:17:58 +0100

    test(infra): add initial pdcl-test project setup

--- test/infra/variables.tf
+++ test/infra/variables.tf
@@ -1,15 +1,2 @@
-variable "project" { }
-
 variable "credentials_file" { }
 
-variable "region" {
-  default = "us-central1"
-}
-
-variable "zone" {
-  default = "us-central1-c"
-}
-
-variable "ipfs_image_tag" { }
-
-variable "sentinel_image_name" {}
