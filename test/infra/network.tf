8366c84e6450078234db2eda085bd745efb0b554
Arkadiusz Noster <x-arkadiusz.noster@upvest.co>
2022-01-11 19:14:35 +0100

    test(infra): add ipfs server deployment

---
+++ test/infra/network.tf
@@ -0,0 +1,21 @@
+resource "google_compute_network" "pdcl-test-network" {
+  name                    = "pdcl-test-network"
+  auto_create_subnetworks = false
+}
+resource "google_compute_subnetwork" "pdcl-test-subnetwork" {
+  name          = "test-subnetwork-${local.region}"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = local.region
+  network       = google_compute_network.pdcl-test-network.id
+}
+
+resource "google_compute_firewall" "allow-ssh-to-ipfs-server" {
+  name = "ssh-ipfs"
+  network = google_compute_network.pdcl-test-network.name
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+  target_tags = ["ipfs-server"]
+  source_ranges = ["0.0.0.0/0"]
+}
