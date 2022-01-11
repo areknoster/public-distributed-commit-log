e3d2a5c9d557392eb0c41f57fc2fe852b1f8d1d6
Arkadiusz Noster <x-arkadiusz.noster@upvest.co>
2022-01-11 19:14:35 +0100

    test(infra): add ipfs server deployment

--- test/infra/locals.tf
+++ test/infra/locals.tf
@@ -2,6 +2,6 @@ locals {
   project             = "pdcl-testing"
   region              = "europe-west6"
   zone                = "europe-west6-b"
-  ipfs_image_tag      = "master-2022-01-06-2a871ef"
-  sentinel_image_name = "acceptance-sentinel"
+  ipfs-image-tag      = "master-2022-01-06-2a871ef"
+  sentinel-image-name = "acceptance-sentinel"
 }
