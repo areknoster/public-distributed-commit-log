f3e18b3b3b138699282ec76dfa6bc6bc2ac18bcb
Arkadiusz Noster <x-arkadiusz.noster@upvest.co>
2022-01-14 00:02:27 +0100

    test(acceptance-sentinel): adjust acceptance-sentinel for getting secret from GCP

--- ipns/keys.go
+++ ipns/keys.go
@@ -9,19 +9,23 @@ import (
 	ipfscrypto "github.com/libp2p/go-libp2p-core/crypto"
 )
 
-func ReadECKeys(privKeyPath string) (ipfscrypto.PrivKey, ipfscrypto.PubKey, error) {
+func ParseKeyPair(pemContent []byte) (ipfscrypto.PrivKey, ipfscrypto.PubKey, error) {
+	block, _ := pem.Decode(pemContent)
+	if block == nil {
+		return nil, nil, fmt.Errorf("failed to parse PEM block containing the private key")
+	}
+	priv, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to parse PEM block containing the private key: %w", err)
+	}
+	return ipfscrypto.KeyPairFromStdKey(priv)
+}
+
+func ReadKeyPair(privKeyPath string) (ipfscrypto.PrivKey, ipfscrypto.PubKey, error) {
 	pemContent, err := os.ReadFile(privKeyPath)
 	if err != nil {
 		return nil, nil, fmt.Errorf("read %s file content: %w", privKeyPath, err)
 	}
 
-	block, _ := pem.Decode(pemContent)
-	if block == nil {
-		return nil, nil, fmt.Errorf("failed to parse PEM block containing the private key")
-	}
-	priv, err := x509.ParseECPrivateKey(block.Bytes)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to parse PEM block containing the private key: %w", err)
-	}
-	return ipfscrypto.KeyPairFromStdKey(priv)
+	return ParseKeyPair(pemContent)
 }
