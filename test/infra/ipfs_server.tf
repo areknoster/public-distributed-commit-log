83dd780ba00565a11836a529cadab970195378e8
Arkadiusz Noster <x-arkadiusz.noster@upvest.co>
2022-01-14 00:01:48 +0100

    test(infra): add definitions for IPNS secret

--- test/infra/ipfs_server.tf
+++ test/infra/ipfs_server.tf
@@ -68,7 +68,7 @@ resource "google_compute_address" "external-ipfs-server-address" {
 resource "google_compute_instance" "ipfs-server-vm" {
   project                   = local.project
   name                      = local.ipfs-server-name
-  machine_type              = "g1-small"
+  machine_type              = "f1-micro"
   zone                      = local.zone
   allow_stopping_for_update = true
 
